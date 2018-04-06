9c8c8bcb431cb8825c392f45afb8dab88d05f688
Seth Puckett <puckett.seth@gmail.com>
2018-04-06 10:53:55 -0400

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Nancy.Owin;
+
+namespace micro_shopping_cart
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseOwin(buildFunc => buildFunc.UseNancy());
+        }
+    }
+}
